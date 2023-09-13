f98e896376046b299714cf72d6b91989f1ae03af
Tano Paul <tanopaul38@gmail.com>
2023-09-12 22:57:56 -0400

    completed comma lab python

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,14 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return " and ".join(items)
+    else:
+        comma_values = ", ".join(items[0: -1])
+        and_value = ", and " + items[-1]
+        sentence = comma_values + and_value
+        print(sentence)
+
+    return sentence
+
+oxford_comma(["fiddleheads", "okra", "kohlrabi"])
